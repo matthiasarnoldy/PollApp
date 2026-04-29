d67929452fc075d54d009b160574a0251b1a6ab3
Matthias Arnoldy <m.arnoldy@outlook.de>
2026-04-29 08:41:51 +0200

    refactor: code structure for improved readability and maintainability

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './shared/components/home/home';
+import { Header } from './shared/components/header/header';
 
 export const routes: Routes = [
     {path: "", component: Home},
     {path: "home", component: Home},
+    {path: "survey/create", component: Header},
 ];
