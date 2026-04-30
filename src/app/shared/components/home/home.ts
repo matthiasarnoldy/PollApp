c08162afed2f74263a3ad851e1c6b506c4f9a572
Matthias Arnoldy <m.arnoldy@outlook.de>
2026-04-30 08:45:33 +0200

    feat: enhance layout and styles for hero and highlights components; add surveys component

--- src/app/shared/components/home/home.ts
+++ src/app/shared/components/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from "../header/header";
 import { Overview } from "../overview/overview";
+import { Surveys } from "../surveys/surveys";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Overview],
+  imports: [Header, Overview, Surveys],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
---
+++ src/app/shared/components/surveys/surveys.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-surveys',
+  imports: [],
+  templateUrl: './surveys.html',
+  styleUrl: './surveys.scss',
+})
+export class Surveys {}
