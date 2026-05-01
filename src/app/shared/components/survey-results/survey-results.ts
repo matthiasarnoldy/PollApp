86416a4a190d58bf18e01e20e58b02cb9b355189
Matthias Arnoldy <m.arnoldy@outlook.de>
2026-05-01 19:20:26 +0200

    feat: add survey results component; implement HTML structure and styles for displaying survey results

---
+++ src/app/shared/components/survey-results/survey-results.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-results',
+  imports: [],
+  templateUrl: './survey-results.html',
+  styleUrl: './survey-results.scss',
+})
+export class SurveyResults {}
--- src/app/shared/components/survey-view/survey-view.ts
+++ src/app/shared/components/survey-view/survey-view.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from "../header/header";
 import { SurveyViewDetail } from "../survey-view-detail/survey-view-detail";
+import { SurveyResults } from "../survey-results/survey-results";
 
 @Component({
   selector: 'app-survey-view',
-  imports: [Header, SurveyViewDetail],
+  imports: [Header, SurveyViewDetail, SurveyResults],
   templateUrl: './survey-view.html',
   styleUrl: './survey-view.scss',
 })
