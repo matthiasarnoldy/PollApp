9c0bfaccd9d5749552dd53c47bef65c8970e4c39
Matthias Arnoldy <m.arnoldy@outlook.de>
2026-05-01 11:24:49 +0200

    feat: implement survey view component; add detail section and styles for improved layout

---
+++ src/app/shared/components/survey-view-detail/survey-view-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-view-detail',
+  imports: [],
+  templateUrl: './survey-view-detail.html',
+  styleUrl: './survey-view-detail.scss',
+})
+export class SurveyViewDetail {}
--- src/app/shared/components/survey-view/survey-view.ts
+++ src/app/shared/components/survey-view/survey-view.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from "../header/header";
+import { SurveyViewDetail } from "../survey-view-detail/survey-view-detail";
 
 @Component({
   selector: 'app-survey-view',
-  imports: [Header],
+  imports: [Header, SurveyViewDetail],
   templateUrl: './survey-view.html',
   styleUrl: './survey-view.scss',
 })
