8503f1da99bc0639dd9193b0a9aa0ed446350b0d
Matthias Arnoldy <m.arnoldy@outlook.de>
2026-05-02 08:46:49 +0200

    feat: implement survey creation component; add form structure and styles for creating new surveys

---
+++ src/app/shared/components/survey-create-form/survey-create-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-create-form',
+  imports: [],
+  templateUrl: './survey-create-form.html',
+  styleUrl: './survey-create-form.scss',
+})
+export class SurveyCreateForm {}
--- src/app/shared/components/survey-create/survey-create.ts
+++ src/app/shared/components/survey-create/survey-create.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from "../header/header";
+import { SurveyCreateForm } from "../survey-create-form/survey-create-form";
 
 @Component({
   selector: 'app-survey-create',
-  imports: [Header],
+  imports: [Header, SurveyCreateForm],
   templateUrl: './survey-create.html',
   styleUrl: './survey-create.scss',
 })
